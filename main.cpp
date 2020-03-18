edd996e23f0b8b4afe42136b51b3bd23132653a2
Aron Friedman <57732827+friedron@users.noreply.github.com>
2020-03-18 18:14:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,5 +6,7 @@ using namespace std;
 int main(){
   cout << "This is just a test" << endl;
   cout << "And it's just a little changing" << endl;
+  cout << "This is total bullshit" << endl;
+  cout << "Pleas cut this out" << endl;
   return 0;
 }
